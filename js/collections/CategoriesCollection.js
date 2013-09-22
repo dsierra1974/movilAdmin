91f26f2cce84e97cc5af6bcf3c8cb7875f007f3f
dsierra1974 <doswaldo74@gmail.com>
2013-09-22 18:12:49 -0500

    Eliminacion de Basura

--- js/collections/CategoriesCollection.js
+++ js/collections/CategoriesCollection.js
@@ -7,7 +7,7 @@ define([ "jquery","backbone","models/CategoryModel" ], function( $, Backbone, Ca
         // The Collection constructor
         initialize: function( models, options ) {
             this.types = options.type;
-            this.url = "http://216.224.166.246:3001/erp/find/"+ this.types +"?query=" + options.query;
+            this.url = "http://168.144.134.243:3000/erp/find/"+ this.types +"?query=" + options.query;
         },
         // Sets the Collection model property to be a Category Model
         model: CategoryModel
